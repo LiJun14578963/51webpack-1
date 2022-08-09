469d170a259a32e42d5131c14cdbad170dae6c15
lijun <1094453726@qq.com>
2022-08-09 15:56:17 +0800

    init

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,45 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',//默认把该地址下的js转义成为dist文件下的filename  main.js
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
+  entry: './src/index.js',
   output: {
-    //path: path.resolve(__dirname, 'dist'),   默认为dist 文件 
     filename: '[name].[contenthash].js',
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+    title: 'XDML - 写代码啦',
+    template: 'src/assets/index.html'
+  }),
+  new MiniCssExtractPlugin({
+    filename: "[name].css",
+    chunkFilename: "[id].css",
+    ignoreOrder:false,
+  }), 
+],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '../',
+              hmr: process.env.NODE_ENV === 'development',
+            },
+          },
+          'css-loader',
+        ],
+        //use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
+
