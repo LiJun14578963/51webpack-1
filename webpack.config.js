471fdfbb67138517e617c1c831b013356999953f
lijun <1094453726@qq.com>
2022-08-10 10:36:41 +0800

    第三次更新

--- webpack.config.js
+++ webpack.config.js
@@ -2,30 +2,17 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
+const base = require('./webpack.config.base.js')
+
 module.exports = {
-  mode: 'development',
+  ...base,
   devtool: 'inline-source-map',
   devServer: {
     contentBase: './dist',
   },
-  entry: './src/index.js',
-  output: {
-    filename: '[name].[contenthash].js',
-  },
-  
-  plugins: [
-    new HtmlWebpackPlugin({
-    title: 'XDML - 写代码啦',
-    template: 'src/assets/index.html'
-  }),
-  new MiniCssExtractPlugin({
-    filename: "[name].[contenthash].css",
-    chunkFilename: "[id].[contenthash].css",
-    ignoreOrder:false,
-  }), 
-],
-  module: {
+    module: {
     rules: [
+      ... base.module.rules,
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,21 +2,13 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
+const base = require('./webpack.config.base.js')
+
 module.exports = {
+  ...base,
   mode: 'production',
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist',
-  },
-  entry: './src/index.js',
-  output: {
-    filename: '[name].[contenthash].js',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-    title: 'XDML - 写代码啦',
-    template: 'src/assets/index.html'
-  }),
+   plugins: [
+    ...base.plugins,
   new MiniCssExtractPlugin({
     filename: "[name].[contenthash].css",
     chunkFilename: "[id].[contenthash].css",
@@ -24,6 +16,7 @@ module.exports = {
   }), 
 ],
   module: {
+    ...base.module.rules,
     rules: [
       {
         test: /\.css$/i,
